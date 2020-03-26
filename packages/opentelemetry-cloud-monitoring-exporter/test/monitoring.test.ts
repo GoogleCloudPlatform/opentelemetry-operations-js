ac896e30599ef2b1920b6f74407e12516101ce56
Mayur Kale <mayurkale@google.com>
2020-03-26 10:19:46 -0700

    feat: add cloud monitoring exporter

---
+++ packages/opentelemetry-cloud-monitoring-exporter/test/monitoring.test.ts
@@ -0,0 +1,145 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as assert from 'assert';
+import * as nock from 'nock';
+import * as sinon from 'sinon';
+import { StackdriverMetricExporter } from '../src';
+import { ConsoleLogger, LogLevel } from '@opentelemetry/core';
+import { ExportResult } from '@opentelemetry/base';
+import { MeterProvider } from '@opentelemetry/metrics';
+import { Labels } from '@opentelemetry/api';
+
+describe('StackdriverMetricExporter', () => {
+  beforeEach(() => {
+    process.env.GCLOUD_PROJECT = 'not-real';
+    nock.disableNetConnect();
+  });
+
+  describe('constructor', () => {
+    it('should construct an exporter', () => {
+      const exporter = new StackdriverMetricExporter();
+      assert.ok(typeof exporter.export === 'function');
+      assert.ok(typeof exporter.shutdown === 'function');
+    });
+
+    it('should construct an exporter', async () => {
+      const exporter = new StackdriverMetricExporter({
+        credentials: {
+          client_email: 'noreply@fake.example.com',
+          private_key: 'this is a key',
+        },
+      });
+
+      assert(exporter);
+      return (exporter['_projectId'] as Promise<string>).then(id => {
+        assert.deepStrictEqual(id, 'not-real');
+      });
+    });
+  });
+
+  describe('export', () => {
+    let exporter: StackdriverMetricExporter;
+    let logger: ConsoleLogger;
+    /* tslint:disable no-any */
+    let metricDescriptors: sinon.SinonSpy<[any, any], any>;
+    let debug: sinon.SinonSpy;
+    let info: sinon.SinonSpy;
+    let warn: sinon.SinonSpy;
+    let error: sinon.SinonSpy;
+    let getClientShouldFail: boolean;
+
+    beforeEach(() => {
+      getClientShouldFail = false;
+      logger = new ConsoleLogger(LogLevel.ERROR);
+      exporter = new StackdriverMetricExporter({
+        logger,
+      });
+
+      metricDescriptors = sinon.spy(
+        /* tslint:disable no-any */
+        (request: any, callback: (err: Error | null) => void): any => {
+          callback(null);
+        }
+      );
+
+      sinon.replace(
+        StackdriverMetricExporter['_monitoring'].projects.metricDescriptors,
+        'create',
+        /* tslint:disable no-any */
+        metricDescriptors as any
+      );
+
+      sinon.replace(exporter['_auth'], 'getClient', () => {
+        if (getClientShouldFail) {
+          throw new Error('fail');
+        }
+        /* tslint:disable no-any */
+        return {} as any;
+      });
+
+      debug = sinon.spy();
+      info = sinon.spy();
+      warn = sinon.spy();
+      error = sinon.spy();
+      sinon.replace(logger, 'debug', debug);
+      sinon.replace(logger, 'info', info);
+      sinon.replace(logger, 'warn', warn);
+      sinon.replace(logger, 'error', error);
+    });
+
+    afterEach(() => {
+      nock.restore();
+      sinon.restore();
+    });
+
+    it('should return not retryable if project id missing', async () => {
+      await exporter['_projectId'];
+      exporter['_projectId'] = undefined;
+
+      const result = await new Promise((resolve, reject) => {
+        exporter.export([], result => {
+          resolve(result);
+        });
+      });
+
+      assert.deepStrictEqual(result, ExportResult.FAILED_NOT_RETRYABLE);
+    });
+
+    it('should export metrics', async () => {
+      const meter = new MeterProvider().getMeter('test-meter');
+      const labels: Labels = { ['keyb']: 'value2', ['keya']: 'value1' };
+      const labelSet = meter.labels(labels);
+      const counter = meter.createCounter('name', {
+        labelKeys: ['keya', 'keyb'],
+      });
+      counter.bind(labelSet).add(10);
+      meter.collect();
+      const records = meter.getBatcher().checkPointSet();
+
+      const result = await new Promise((resolve, reject) => {
+        exporter.export(records, result => {
+          resolve(result);
+        });
+      });
+      console.log(metricDescriptors.getCall(0).args[0]);
+      assert.deepStrictEqual(
+        metricDescriptors.getCall(0).args[0].resource.type,
+        'custom.googleapis.com/opentelemetry/name'
+      );
+
+      assert.deepStrictEqual(result, ExportResult.SUCCESS);
+    });
+  });
+});
---
+++ packages/opentelemetry-cloud-monitoring-exporter/test/transform.test.ts
@@ -0,0 +1,174 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as assert from 'assert';
+import {
+  TEST_ONLY,
+  transformMetricDescriptor,
+  createTimeSeries,
+} from '../src/transform';
+import {
+  MetricKind as OTMetricKind,
+  MetricDescriptor as OTMetricDescriptor,
+  MeterProvider,
+} from '@opentelemetry/metrics';
+import { ValueType as OTValueType, Labels } from '@opentelemetry/api';
+import { MetricKind, ValueType, MetricDescriptor } from '../src/types';
+
+describe('transform', () => {
+  const METRIC_NAME = 'metric-name';
+  const METRIC_DESCRIPTION = 'metric-description';
+  const DEFAULT_UNIT = '1';
+  const labelKeys: string[] = ['key1', 'key2'];
+
+  describe('MetricDescriptor', () => {
+    const metricDescriptor: OTMetricDescriptor = {
+      name: METRIC_NAME,
+      description: METRIC_DESCRIPTION,
+      unit: DEFAULT_UNIT,
+      metricKind: OTMetricKind.COUNTER,
+      valueType: OTValueType.INT,
+      monotonic: true,
+      labelKeys,
+    };
+    const metricDescriptor1: OTMetricDescriptor = {
+      name: METRIC_NAME,
+      description: METRIC_DESCRIPTION,
+      unit: DEFAULT_UNIT,
+      metricKind: OTMetricKind.OBSERVER,
+      valueType: OTValueType.DOUBLE,
+      monotonic: false,
+      labelKeys,
+    };
+
+    it('should return a Stackdriver MetricKind', () => {
+      assert.strictEqual(
+        TEST_ONLY.transformMetricKind(OTMetricKind.COUNTER),
+        MetricKind.CUMULATIVE
+      );
+      assert.strictEqual(
+        TEST_ONLY.transformMetricKind(OTMetricKind.OBSERVER),
+        MetricKind.GAUGE
+      );
+      assert.strictEqual(
+        TEST_ONLY.transformMetricKind(OTMetricKind.MEASURE),
+        MetricKind.UNSPECIFIED
+      );
+    });
+
+    it('should return a Stackdriver ValueType', () => {
+      assert.strictEqual(
+        TEST_ONLY.transformValueType(OTValueType.INT),
+        ValueType.INT64
+      );
+      assert.strictEqual(
+        TEST_ONLY.transformValueType(OTValueType.DOUBLE),
+        ValueType.DOUBLE
+      );
+    });
+
+    it('should return a Stackdriver LabelDescriptor', () => {
+      assert.deepStrictEqual(TEST_ONLY.transformLabelDescriptor(labelKeys), [
+        { description: 'key1', key: 'key1', valueType: 'STRING' },
+        { description: 'key2', key: 'key2', valueType: 'STRING' },
+        {
+          description: 'OpenTelemetry task identifier',
+          key: 'opentelemetry_task',
+          valueType: 'STRING',
+        },
+      ]);
+    });
+
+    it('should return a Stackdriver DisplayName', () => {
+      assert.strictEqual(
+        TEST_ONLY.transformDisplayName(
+          'custom.googleapis.com/opentelemetry',
+          'demo/latency'
+        ),
+        'custom.googleapis.com/opentelemetry/demo/latency'
+      );
+    });
+
+    it('should return a Stackdriver MetricType', () => {
+      assert.strictEqual(
+        TEST_ONLY.transformMetricType('opentelemetry', 'demo/latency'),
+        'opentelemetry/demo/latency'
+      );
+    });
+
+    it('should return a Cumulative Stackdriver MetricDescriptor', () => {
+      const descriptor: MetricDescriptor = transformMetricDescriptor(
+        metricDescriptor,
+        'custom.googleapis.com/myorg/',
+        'myorg/'
+      );
+
+      assert.strictEqual(descriptor.description, METRIC_DESCRIPTION);
+      assert.strictEqual(descriptor.displayName, `myorg/${METRIC_NAME}`);
+      assert.strictEqual(
+        descriptor.type,
+        `custom.googleapis.com/myorg/${METRIC_NAME}`
+      );
+      assert.strictEqual(descriptor.unit, DEFAULT_UNIT);
+      assert.strictEqual(descriptor.metricKind, MetricKind.CUMULATIVE);
+      assert.strictEqual(descriptor.valueType, ValueType.INT64);
+    });
+
+    it('should return a Gauge Stackdriver MetricDescriptor', () => {
+      const descriptor: MetricDescriptor = transformMetricDescriptor(
+        metricDescriptor1,
+        'custom.googleapis.com/myorg/',
+        'myorg/'
+      );
+
+      assert.strictEqual(descriptor.description, METRIC_DESCRIPTION);
+      assert.strictEqual(descriptor.displayName, `myorg/${METRIC_NAME}`);
+      assert.strictEqual(
+        descriptor.type,
+        `custom.googleapis.com/myorg/${METRIC_NAME}`
+      );
+      assert.strictEqual(descriptor.unit, DEFAULT_UNIT);
+      assert.strictEqual(descriptor.metricKind, MetricKind.GAUGE);
+      assert.strictEqual(descriptor.valueType, ValueType.DOUBLE);
+    });
+  });
+
+  describe('TimeSeries', () => {
+    it('should return a Stackdriver Metric', () => {
+      const meter = new MeterProvider().getMeter('test-meter');
+      const labels: Labels = { ['keyb']: 'value2', ['keya']: 'value1' };
+      const labelSet = meter.labels(labels);
+
+      const counter = meter.createCounter(METRIC_NAME, {
+        description: METRIC_DESCRIPTION,
+        labelKeys: ['keya', 'keyb'],
+      });
+      counter.bind(labelSet).add(10);
+      meter.collect();
+      const [record] = meter.getBatcher().checkPointSet();
+      const ts = createTimeSeries(record, 'otel', new Date().toISOString());
+      assert.strictEqual(ts.metric.type, 'otel/metric-name');
+      assert.strictEqual(ts.metric.labels['keya'], 'value1');
+      assert.strictEqual(ts.metric.labels['keyb'], 'value2');
+      assert.deepStrictEqual(ts.resource, {
+        labels: {},
+        type: 'global',
+      });
+      assert.strictEqual(ts.metricKind, MetricKind.CUMULATIVE);
+      assert.strictEqual(ts.valueType, ValueType.DOUBLE);
+      assert.strictEqual(ts.points.length, 1);
+      assert.deepStrictEqual(ts.points[0].value, { doubleValue: 10 });
+    });
+  });
+});
