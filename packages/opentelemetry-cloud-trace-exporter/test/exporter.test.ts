a8dddbe6bc6dee30a21426b5d93ba0ca34a9e684
shiv <shivkanyaandhare25@gmail.com>
2020-03-29 16:11:44 -0400

    AddAdd q

--- packages/opentelemetry-cloud-trace-exporter/test/exporter.test.ts
+++ packages/opentelemetry-cloud-trace-exporter/test/exporter.test.ts
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* tslint:disable no-any */
+
 import * as types from '@opentelemetry/api';
 import { TraceFlags } from '@opentelemetry/api';
 import { ExportResult } from '@opentelemetry/base';
