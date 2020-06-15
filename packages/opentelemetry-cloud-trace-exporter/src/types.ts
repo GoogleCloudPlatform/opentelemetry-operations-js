f61de9447d6700279e616ad67e36b17226c109e7
reggiemcdonald <regmcdonald95@gmail.com>
2020-06-15 08:54:31 -0600

    types: removed unecessary import

--- packages/opentelemetry-cloud-trace-exporter/src/types.ts
+++ packages/opentelemetry-cloud-trace-exporter/src/types.ts
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Compute, JWT, OAuth2Client } from 'google-auth-library';
-
 export interface Span {
   name?: string;
   spanId?: string;
