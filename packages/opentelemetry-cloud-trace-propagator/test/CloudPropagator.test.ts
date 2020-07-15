64ac397d533d1d028acb42790fba16804015a084
shiv <shivkanyaandhare25@gmail.com>
2020-07-15 15:59:38 -0400

    add inject method test cases

--- packages/opentelemetry-cloud-trace-propagator/test/CloudPropagator.test.ts
+++ packages/opentelemetry-cloud-trace-propagator/test/CloudPropagator.test.ts
@@ -11,3 +11,58 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+import { defaultSetter, SpanContext, TraceFlags } from '@opentelemetry/api';
+import { Context } from '@opentelemetry/context-base';
+import * as assert from 'assert';
+import { setExtractedSpanContext } from '@opentelemetry/core';
+import {
+  CloudPropagator,
+  TRACE_CONTEXT_HEADER_NAME,
+} from '../src/CloudPropagator';
+
+describe('CloudPropagator', () => {
+  const cloudPropagator = new CloudPropagator();
+  let carrier: { [key: string]: unknown };
+
+  beforeEach(() => {
+    carrier = {};
+  });
+  
+  describe('.inject()', () => {
+    it('should inject a context of a sampled span', () => {
+      const spanContext: SpanContext = {
+        traceId: 'd4cda95b652f4a1592b449d5929fda1b',
+        spanId: '6e0c63257de34c92',
+        traceFlags: TraceFlags.SAMPLED,
+      };
+
+      cloudPropagator.inject(
+        setExtractedSpanContext(Context.ROOT_CONTEXT, spanContext),
+        carrier,
+        defaultSetter
+      );
+
+      assert.deepStrictEqual(
+        carrier[TRACE_CONTEXT_HEADER_NAME],
+        'd4cda95b652f4a1592b449d5929fda1b/7929822056569588882;o=1'
+      );
+    });
+    it('should inject a context of a unsampled span', () => {
+      const spanContext: SpanContext = {
+        traceId: 'd4cda95b652f4a1592b449d5929fda1b',
+        spanId: '6e0c63257de34c92',
+        traceFlags: TraceFlags.NONE,
+      };
+      cloudPropagator.inject(
+        setExtractedSpanContext(Context.ROOT_CONTEXT, spanContext),
+        carrier,
+        defaultSetter
+      );
+      assert.deepStrictEqual(
+        carrier[TRACE_CONTEXT_HEADER_NAME],
+        'd4cda95b652f4a1592b449d5929fda1b/7929822056569588882;o=0'
+      );
+    });
+  });
+});
