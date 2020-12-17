5f878e52e27c3989d62fe72bfd3d492443f5f105
Mehran Khajavi <sinoohe.mkh@gmail.com>
2020-12-17 12:27:47 +0330

    adding http mapping

--- packages/opentelemetry-cloud-trace-exporter/src/transform.ts
+++ packages/opentelemetry-cloud-trace-exporter/src/transform.ts
@@ -102,8 +102,8 @@ function transformAttributes(
     serviceAttributes,
     resource.attributes
   );
-
-  const attributeMap = transformAttributeValues(attributes);
+  const changedAttributes = transformAttributeNames(attributes);
+  const attributeMap = transformAttributeValues(changedAttributes);
   return {
     attributeMap,
     // @todo get dropped attribute count from sdk ReadableSpan
@@ -146,4 +146,33 @@ function valueToAttributeValue(
     default:
       return {};
   }
+}
+
+const HTTP_ATTRIBUTE_MAPPING: { [key: string]: string} = {
+  'http.method': '/http/method',
+  'http.url': '/http/url',
+  'http.target': '/http/target',
+  'http.host': '/http/host',
+  'http.scheme': '/http/client_protocol',
+  'http.status_code': '/http/status_code',
+  'http.flavor': '/http/flavor',
+  'http.user_agent': '/http/user_agent',
+  'http.request_content_length': '/http/request_content_length',
+  'http.request_content_length_uncompressed': '/http/request_content_length_uncompressed',
+  'http.response_content_length': '/http/response_content_length',
+  'http.response_content_length_uncompressed': '/http/response_content_length_uncompressed',
+  'http.server_name': '/http/server_name',
+  'http.route': '/http/route',
+  'http.client_ip': '/http/client_ip'
+}
+function transformAttributeNames(attributes: ot.Attributes): ot.Attributes {
+  const out: ot.Attributes = {};
+  for(const [key, value] of Object.entries(attributes)) {
+    if (HTTP_ATTRIBUTE_MAPPING[key]) {
+      out[HTTP_ATTRIBUTE_MAPPING[key]] = value;
+    } else {
+      out[key] = value;
+    }
+  }
+  return out;
 }
