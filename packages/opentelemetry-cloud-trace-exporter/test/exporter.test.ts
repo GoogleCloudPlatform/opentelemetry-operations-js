4ecdeb8b1821dfbad0aff94a063e88c8f4a14cef
Justin Beckwith <justin.beckwith@gmail.com>
2021-01-27 10:47:48 -0800

    poke

--- packages/opentelemetry-cloud-trace-exporter/test/exporter.test.ts
+++ packages/opentelemetry-cloud-trace-exporter/test/exporter.test.ts
@@ -58,9 +58,10 @@ describe('Google Cloud Trace Exporter', () => {
       });
     });
 
-    it('should construct exporter in GCE/GCP environment without args', async () => {
+    it('should construct exporter in GCE/GCP environment without args', async function () {
       // This variable is set by the test env and must be undefined to force
       // a metadata server request.
+      this.timeout(6000);
       delete process.env.GCLOUD_PROJECT;
       const gcpMock = nock(HOST_ADDRESS)
         .get(PROJECT_ID_PATH)
