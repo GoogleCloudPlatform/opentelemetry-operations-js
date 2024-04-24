817b7c35a88b3154b423516a81ccc3eca119d209
Pranav Sharma <sharmapranav@google.com>
2024-04-24 19:55:08 +0000

    feat: Add gRPC sample for OTLP export

---
+++ samples/otlptracegrpc/README.md
@@ -0,0 +1,37 @@
+# Overview
+
+This example shows how to send traces to an OTLP *(OpenTelemetry Protocol)* endpoint that is protected by GCP authentication over gRPC.
+
+## Installation
+
+```sh
+# from root of repo, build all packages
+npm install
+```
+
+## Run the Application
+
+```sh
+# export necessary OTEL environment variables
+export OTEL_RESOURCE_ATTRIBUTES="gcp.project_id=<project-id>"
+export OTEL_EXPORTER_OTLP_ENDPOINT=<endpoint>
+
+# run the app - this starts app at port 8080
+cd samples/otlptracehttp && ts-node app.ts
+```
+
+## Sending Requests to the Application
+
+In another terminal window:
+
+```sh
+curl localhost:8080/rolldice?rolls=12
+```
+
+## Useful links
+- For more information on OpenTelemetry, visit: <https://opentelemetry.io/>
+- For more information on tracing, visit: <https://opentelemetry.io/docs/concepts/signals/traces/>
+
+## LICENSE
+
+Apache License 2.0
---
+++ samples/otlptracegrpc/app.ts
@@ -0,0 +1,67 @@
+// Copyright OpenTelemetry Authors
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+/*app.ts*/
+import { trace } from '@opentelemetry/api';
+import express, { Express, Request, Response } from 'express';
+import { rollTheDice } from './dice';
+
+const tracer = trace.getTracer('dice-server', '0.1.0');
+
+const PORT: number = parseInt(process.env.PORT || '8080');
+const app: Express = express();
+
+import {NodeSDK} from '@opentelemetry/sdk-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
+import { AuthClient, GoogleAuth } from 'google-auth-library';
+import { Headers } from 'google-auth-library/build/src/auth/oauth2client';
+
+async function main() {
+  async function getAuthenticatedClient(): Promise<AuthClient> {  
+    const auth: GoogleAuth = new GoogleAuth({
+      scopes: 'https://www.googleapis.com/auth/cloud-platform',
+    });
+    const client: AuthClient = await auth.getClient();
+    return client;    
+  }
+
+  const authenticatedClient: AuthClient = await getAuthenticatedClient();
+  const headers: Headers = await authenticatedClient.getRequestHeaders();
+
+  const sdk = new NodeSDK({
+    traceExporter: new OTLPTraceExporter({
+      headers: headers,
+    }),
+  });
+  sdk.start();
+
+  app.get('/rolldice', (req: Request, res: Response) => {
+    const rolls = req.query.rolls ? parseInt(req.query.rolls.toString()) : NaN;
+    if (isNaN(rolls)) {
+      res
+          .status(400)
+          .send("Request parameter 'rolls' is missing or not a number.");
+      return;
+    }
+    res.send(JSON.stringify(rollTheDice(rolls, 1, 6)));
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Listening for requests on http://localhost:${PORT}`);
+  });
+}
+
+main();
