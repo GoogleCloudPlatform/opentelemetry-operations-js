aab2936c16e4e6deb5a53a72086b654f73548691
Pranav Sharma <sharmapranav@google.com>
2024-04-26 20:03:03 -0400

    feat: Add sample for otlp-trace export via grpc (#700)
    * feat: Add gRPC sample for OTLP export
    
    * feat: enable debug logs for the sample
    
    * fix: missing credentials
    
    * feat: add client appliction to automate span generation
    
    * docs: add license header
    
    * update config files
    
    * apply auto-fixable lint issues
    
    * Remove verbose logging from app.ts
    
    * Remove inline function
    
    * fix extraneous imports lint
    
    * add missing license header
    
    * pin dependencies and add missing deps
    
    * fix style issues
    
    * Update readme
    
    * Update client to use string template for URL

---
+++ samples/otlptracegrpc/README.md
@@ -0,0 +1,43 @@
+# Overview
+
+This example shows how to send traces to an OTLP *(OpenTelemetry Protocol)* endpoint that is protected by GCP authentication over gRPC.
+
+## Installation
+
+```sh
+# from root of repo, build all packages
+npm install
+```
+
+## Run the Application
+
+```sh
+# export necessary OTEL environment variables
+export OTEL_RESOURCE_ATTRIBUTES="gcp.project_id=<project-id>"
+export OTEL_EXPORTER_OTLP_ENDPOINT=<endpoint>
+
+# run the app - this starts app at port 8080
+cd samples/otlptracegrpc && npm run start
+```
+
+## Sending Requests to the Application
+
+In another terminal window:
+
+To send a one-off request:
+```sh
+curl localhost:8080/rolldice?rolls=12
+```
+
+To send requests indefinitely at a fixed rate, use the provided client application:
+```sh
+npm run client
+```
+
+## Useful links
+- For more information on OpenTelemetry, visit: <https://opentelemetry.io/>
+- For more information on tracing, visit: <https://opentelemetry.io/docs/concepts/signals/traces/>
+
+## LICENSE
+
+Apache License 2.0
---
+++ samples/otlptracegrpc/client.ts
@@ -0,0 +1,40 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Simple script that keeps generating spans by making random rolls via the running sample app.
+import axios, {AxiosResponse} from 'axios';
+
+const INTERVAL_MS = 10000;
+const APP_ENDPOINT = 'http://localhost:8080/rolldice';
+
+async function rollDice() {
+  const numRolls = Math.floor(Math.random() * 10) + 1;
+  console.log(`Making ${numRolls} rolls`);
+  const url = `${APP_ENDPOINT}?rolls=${numRolls}`;
+  try {
+    const response: AxiosResponse = await axios(url);
+    if (response.status !== 200) {
+      console.error('Error making roll', response.status);
+    } else {
+      console.log(response.statusText);
+    }
+  } catch (error) {
+    console.error();
+  }
+}
+
+const intervalId = setInterval(rollDice, 10 * 1000);
+console.log(
+  `Rolling dice every ${INTERVAL_MS} ms with interval ID: ${intervalId}`
+);
